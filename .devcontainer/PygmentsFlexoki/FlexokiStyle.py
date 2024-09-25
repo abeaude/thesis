5f5ab6e8b16249bd0fe7d2bc602322a243edaf35
Aurélien BEAUDE <beaude.aurelien@orange.fr>
2024-09-25 18:40:20 +0200

    Update

--- .devcontainer/PygmentsFlexoki/FlexokiStyle.py
+++ .devcontainer/PygmentsFlexoki/FlexokiStyle.py
@@ -1,6 +1,6 @@
 from pygments.style import Style
 from pygments.token import Token, Comment, Keyword, Name, String, Error, Generic, Number, Operator, Punctuation
-
+# https://pygments.org/docs/tokens/
 class FlexokiStyle(Style):
     name = "Flexoki"
     background_color = "#FFFCF0"
@@ -8,15 +8,33 @@ class FlexokiStyle(Style):
         Comment: "#B7B5AC",
         Punctuation: "#6F6E69",
         
-        Operator: 'bold #6F6E69',
+        Operator: '#AF3029',
+        Operator.Word: "bold #AF3029",
         
+        # Name: "",
         Name.Function: "bold #BC5215",
+        Name.Function.Magic: "",
         Name.Builtin: "#24837B",
+        Name.Builtin.Pseudo: "",
         Name.Decorator: "bold #AD8301",
+        Name.Class: "#BC5215",
+        Name.Constant: "#AD8301",
+        Name.Exception: "#AF3029",
+        # Name.Namespace: "",
         
-        Keyword: "#205EA6",
+        
+        Keyword: "#66800B",
         Keyword.Namespace: "#AF3029",
+        Keyword.Constant: "#AD8301",
+        Keyword.Reserved: "#A02F6F",
+        # Keyword.Type: "",
         
         Number: "#5E409D",
         String: "#24837B",
+        # String.Doc: "",
+        # String.Double: "",
+        # String.Single: "",
+        # String.Escape: "",
+        # String.Interpol: "",
+
     }
